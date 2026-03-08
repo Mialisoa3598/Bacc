b37a143b4854b017e0074e2480669e241e6d6a3f
Mialisoa3598 <habbymialisoa07@gmail.com>
2026-03-08 11:42:36 +0300

    [feature/script] script base, test de donnees, delete

---
+++ sql/delete.sql
@@ -0,0 +1,8 @@
+DELETE FROM t_note_finale;
+DELETE FROM t_note;
+DELETE FROM t_parametre;
+DELETE FROM t_matiere;
+DELETE FROM t_operateur;
+DELETE FROM t_resolution;
+DELETE FROM t_candidat;
+DELETE FROM t_correcteur;
