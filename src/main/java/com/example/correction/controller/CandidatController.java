993a0dc8f582fafb4dae0fbf61840b51c0d4ea31
Mialisoa3598 <habbymialisoa07@gmail.com>
2026-03-14 17:43:23 +0300

    [s2-modifier-regle] controller-correction

--- src/main/java/com/example/correction/controller/CandidatController.java
+++ src/main/java/com/example/correction/controller/CandidatController.java
@@ -1,10 +1,12 @@
 package com.example.correction.controller;
 
 import com.example.correction.model.Candidat;
+// import com.example.correction.model.Correcteur;
 import com.example.correction.service.CandidatService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/candidats")
@@ -15,33 +17,66 @@ public class CandidatController {
         this.service = service;
     }
 
+    // @GetMapping
+    // public String list(Model model) {
+    //     model.addAttribute("candidats", service.findAll());
+    //     return "candidat/list";
+    // }
+
     @GetMapping
-    public String list(Model model) {
-        model.addAttribute("candidats", service.findAll());
-        return "candidat/list";
+    public ModelAndView list() {
+        ModelAndView mav = new ModelAndView("candidat/list");
+        mav.addObject("candidats", service.findAll());
+        return mav;
     }
+
+    // @GetMapping("/new")
+    // public String newForm(Model model) {
+    //     model.addAttribute("candidat", new Candidat());
+    //     return "candidat/form";
+    // }
 
     @GetMapping("/new")
-    public String newForm(Model model) {
-        model.addAttribute("candidat", new Candidat());
-        return "candidat/form";
+    public ModelAndView newForm() {
+        ModelAndView mav = new ModelAndView("candidat/form");
+        mav.addObject("candidat", new Candidat());
+        return mav;
     }
+
+    // @GetMapping("/edit/{id}")
+    // public String editForm(@PathVariable Long id, Model model) {
+    //     model.addAttribute("candidat", service.findById(id));
+    //     return "candidat/form";
+    // }
 
     @GetMapping("/edit/{id}")
-    public String editForm(@PathVariable Long id, Model model) {
-        model.addAttribute("candidat", service.findById(id));
-        return "candidat/form";
+    public ModelAndView editForm(@PathVariable Long id) {
+        ModelAndView mav = new ModelAndView("candidat/form");
+        mav.addObject("candidat", service.findById(id));
+        return mav;
     }
+
+    // @PostMapping("/save")
+    // public String save(@ModelAttribute Candidat candidat) {
+    //     service.save(candidat);
+    //     return "redirect:/candidats";
+    // }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute Candidat candidat) {
+    public ModelAndView save(@ModelAttribute Candidat candidat) {
         service.save(candidat);
-        return "redirect:/candidats";
+        return new ModelAndView("redirect:/candidats");
     }
 
+    // @GetMapping("/delete/{id}")
+    // public String delete(@PathVariable Long id) {
+    //     service.delete(id);
+    //     return "redirect:/candidats";
+    // }
+
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Long id) {
+    public ModelAndView delete(@PathVariable Long id) {
         service.delete(id);
-        return "redirect:/candidats";
+        return new ModelAndView("redirect:/candidats");
     }
 }
--- src/main/java/com/example/correction/controller/ParametreController.java
+++ src/main/java/com/example/correction/controller/ParametreController.java
@@ -3,8 +3,9 @@ package com.example.correction.controller;
 import com.example.correction.model.Parametre;
 import com.example.correction.service.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/parametres")
@@ -24,39 +25,78 @@ public class ParametreController {
         this.resolutionService = resolutionService;
     }
 
+    // @GetMapping
+    // public String list(Model model) {
+    //     model.addAttribute("parametres", service.findAll());
+    //     return "parametre/list";
+    // }
+
     @GetMapping
-    public String list(Model model) {
-        model.addAttribute("parametres", service.findAll());
-        return "parametre/list";
+    public ModelAndView list() {
+        ModelAndView mav = new ModelAndView("parametre/list");
+        mav.addObject("parametres", service.findAll());
+        return mav;
     }
+
+    // @GetMapping("/new")
+    // public String newForm(Model model) {
+    //     model.addAttribute("parametre", new Parametre());
+    //     model.addAttribute("matieres", matiereService.findAll());
+    //     model.addAttribute("operateurs", operateurService.findAll());
+    //     model.addAttribute("resolutions", resolutionService.findAll());
+    //     return "parametre/form";
+    // }
 
     @GetMapping("/new")
-    public String newForm(Model model) {
-        model.addAttribute("parametre", new Parametre());
-        model.addAttribute("matieres", matiereService.findAll());
-        model.addAttribute("operateurs", operateurService.findAll());
-        model.addAttribute("resolutions", resolutionService.findAll());
-        return "parametre/form";
+    public ModelAndView newForm() {
+        ModelAndView mav = new ModelAndView("parametre/form");
+        mav.addObject("parametre", new Parametre());
+        mav.addObject("matieres", matiereService.findAll());
+        mav.addObject("operateurs", operateurService.findAll());
+        mav.addObject("resolutions", resolutionService.findAll());
+        return mav;
     }
+
+    // @GetMapping("/edit/{id}")
+    // public String editForm(@PathVariable Long id, Model model) {
+    //     model.addAttribute("parametre", service.findById(id));
+    //     model.addAttribute("matieres", matiereService.findAll());
+    //     model.addAttribute("operateurs", operateurService.findAll());
+    //     model.addAttribute("resolutions", resolutionService.findAll());
+    //     return "parametre/form";
+    // }
 
     @GetMapping("/edit/{id}")
-    public String editForm(@PathVariable Long id, Model model) {
-        model.addAttribute("parametre", service.findById(id));
-        model.addAttribute("matieres", matiereService.findAll());
-        model.addAttribute("operateurs", operateurService.findAll());
-        model.addAttribute("resolutions", resolutionService.findAll());
-        return "parametre/form";
+    public ModelAndView editForm(@PathVariable Long id) {
+        ModelAndView mav = new ModelAndView("parametre/form");
+        mav.addObject("parametre", service.findById(id));
+        mav.addObject("matieres", matiereService.findAll());
+        mav.addObject("operateurs", operateurService.findAll());
+        mav.addObject("resolutions", resolutionService.findAll());
+        return mav;
     }
+
+    // @PostMapping("/save")
+    // public String save(@ModelAttribute Parametre parametre) {
+    //     service.save(parametre);
+    //     return "redirect:/parametres";
+    // }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute Parametre parametre) {
+    public ModelAndView save(@ModelAttribute Parametre parametre) {
         service.save(parametre);
-        return "redirect:/parametres";
+        return new ModelAndView("redirect:/parametres");
     }
 
+    // @GetMapping("/delete/{id}")
+    // public String delete(@PathVariable Long id) {
+    //     service.delete(id);
+    //     return "redirect:/parametres";
+    // }
+
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Long id) {
+    public ModelAndView delete(@PathVariable Long id) {
         service.delete(id);
-        return "redirect:/parametres";
+        return new ModelAndView("redirect:/parametres");
     }
 }
